a7574f13e0736e9a5d2d65e9c71aa060402cabc4
Harshal Tupe <harshaltupe12@gmail.com>
2024-07-03 13:30:13 +0530

    Page for dynamic routing

---
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+interface PROPS{
+  params:{
+    'template-slug':string
+  }
+}
+
+function CreateNewContent(props:PROPS) {
+  return (
+    <div>
+      {/* Form Section */}
+
+      {/* Output Section */}
+    </div>
+  )
+}
+
+export default CreateNewContent
