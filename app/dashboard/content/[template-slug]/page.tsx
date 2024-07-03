ff2e40a421cb2a86976e6267d21015407bfd16f6
Harshal Tupe <harshaltupe12@gmail.com>
2024-07-03 19:59:42 +0530

    Content generating page

--- app/dashboard/content/[template-slug]/page.tsx
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -1,19 +1,47 @@
-import React from 'react'
+"use client";
+import React from "react";
+import FormSection from "../_component/FormSection";
+import OutputSection from "../_component/OutputSection";
+import { TEMPLATE } from "../../_components/TemplateListSection";
+import Template from "@/app/(data)/Template";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 
-interface PROPS{
-  params:{
-    'template-slug':string
-  }
+interface PROPS {
+  params: {
+    "template-slug": string;
+  };
 }
 
-function CreateNewContent(props:PROPS) {
+function CreateNewContent(props: PROPS) {
+  const selectedTemplate: TEMPLATE | undefined = Template?.find(
+    (item) => item.slug == props.params["template-slug"]
+  );
+
+  const GenerateAIContent = (formData: any) => {};
   return (
-    <div>
-      {/* Form Section */}
+    <div className="p-5">
+      <Link href={"/dashboard"}>
+        <Button>
+          <ArrowLeft /> Back
+        </Button>
+      </Link>
 
-      {/* Output Section */}
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5 py-5">
+        {/* Form Section */}
+        <FormSection
+          selectedTemplate={selectedTemplate}
+          userFormInput={(v: any) => GenerateAIContent(v)}
+        />
+
+        {/* Output Section */}
+        <div className="col-span-2">
+          <OutputSection />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default CreateNewContent
+export default CreateNewContent;
