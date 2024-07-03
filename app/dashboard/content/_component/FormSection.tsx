55bd3f83cd59e54911306424a2e7b8fc067f46e3
Harshal Tupe <harshaltupe12@gmail.com>
2024-07-03 19:59:57 +0530

    Dynamic form

--- app/dashboard/content/_component/FormSection.tsx
+++ app/dashboard/content/_component/FormSection.tsx
@@ -1,9 +1,65 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import { TEMPLATE } from "../../_components/TemplateListSection";
+import Image from "next/image";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
 
-function FormSection() {
-  return (
-    <div>FormSection</div>
-  )
+interface PROPS {
+  selectedTemplate?: TEMPLATE;
+  userFormInput: any
 }
 
-export default FormSection
+function FormSection({ selectedTemplate, userFormInput }: PROPS) {
+
+
+    const [formData, setFormData] = useState<any>();
+
+
+  const handleInputChnage = (event: any) => {
+    const {name,value}=event.target;
+    setFormData({...formData, [name]:value});
+  };
+
+  const onSubmit = (e: any) => {
+    e.preventDefault();
+    userFormInput(formData)
+  };
+  return (
+    <div className="p-5 shadow-lg border rounded-lg bg-white">
+      {/* @ts-ignore */}
+      <Image src={selectedTemplate?.icon} alt="icon" width={70} height={70} />
+      <h2 className="font-bold text-2xl mb-2 text-primary">
+        {selectedTemplate?.name}
+      </h2>
+      <p className="text-gray-500 text-sm">{selectedTemplate?.desc}</p>
+
+      <form className="mt-6" onSubmit={onSubmit}>
+        {selectedTemplate?.form?.map((item, index) => (
+          <div className="my-2 flex flex-col gap-2 mb-7">
+            <label className="font-bold">{item.label}</label>
+            {item.field == "input" ? (
+              <Input
+                name={item.name}
+                required={item?.required}
+                onChange={handleInputChnage}
+              />
+            ) : item.field == "textarea" ? (
+              <Textarea
+                name={item.name}
+                required={item?.required}
+                onChange={handleInputChnage}
+              />
+            ) : null}
+          </div>
+        ))}
+        <Button type="submit" className="w-full py-6">
+          Generate Content
+        </Button>
+      </form>
+    </div>
+  );
+}
+
+export default FormSection;
