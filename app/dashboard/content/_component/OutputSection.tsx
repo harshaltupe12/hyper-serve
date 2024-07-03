a5f36e33bc6a5d2a67a4eacb994be3e9476aa584
Harshal Tupe <harshaltupe12@gmail.com>
2024-07-03 20:00:16 +0530

    Text editor

---
+++ app/dashboard/content/_component/OutputSection.tsx
@@ -0,0 +1,32 @@
+"use client";
+import React, { useRef } from "react";
+import "@toast-ui/editor/dist/toastui-editor.css";
+
+import { Editor } from "@toast-ui/react-editor";
+import { Button } from "@/components/ui/button";
+import { Copy } from "lucide-react";
+
+function OutputSection() {
+  const editorRef: any = useRef();
+  return (
+    <div className="bg-white shadow-lg border rounded-lg">
+      <div className="flex justify-between items-center p-5">
+        <h2 className="font-medium text-lg">Your Result</h2>
+        <Button className="flex gap-2">
+          <Copy className="w-4 h-4"/>
+          Copy
+        </Button>
+      </div>
+      <Editor
+        ref={editorRef}
+        initialValue="Your result will appear here..."
+        height="550px"
+        initialEditType="wysiwyg"
+        useCommandShortcut={true}
+        onChange={() => console.log(editorRef.current.getInstance().getMarkdown())}
+      />
+    </div>
+  );
+}
+
+export default OutputSection;
