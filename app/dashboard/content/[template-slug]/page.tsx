5cffecea43d954220f1d24844084393a30a11f1f
Harshal Tupe <harshaltupe12@gmail.com>
2024-07-04 14:52:59 +0530

    Storing and generating data functionality

--- app/dashboard/content/[template-slug]/page.tsx
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import FormSection from "../_component/FormSection";
 import OutputSection from "../_component/OutputSection";
 import { TEMPLATE } from "../../_components/TemplateListSection";
@@ -7,6 +7,11 @@ import Template from "@/app/(data)/Template";
 import { Button } from "@/components/ui/button";
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
+import { chatSession } from "@/utils/AiModel";
+import { db } from "@/utils/db";
+import { AIOutput } from "@/utils/schema";
+import { useUser } from "@clerk/nextjs";
+import moment from "moment";
 
 interface PROPS {
   params: {
@@ -19,7 +24,33 @@ function CreateNewContent(props: PROPS) {
     (item) => item.slug == props.params["template-slug"]
   );
 
-  const GenerateAIContent = (formData: any) => {};
+  const [loading, setLoading] = useState(false);
+  const [aiOutput, setAiOutput] = useState<string>('');
+  const {user} = useUser();
+
+  const GenerateAIContent = async(formData: any) => {
+    setLoading(true);
+    const SelectedPrompt=selectedTemplate?.aiPrompt;
+    const FinalAIPrompt = JSON.stringify(formData)+", "+SelectedPrompt;
+
+    const result=await chatSession.sendMessage(FinalAIPrompt);
+    console.log(result.response.text());
+    setAiOutput(result?.response.text())
+    await SaveInDb(JSON.stringify(formData),selectedTemplate?.slug,result?.response.text())
+    setLoading(false);
+  };
+
+  const SaveInDb=async(formData:any,slug:any,aiResp:string)=>{
+    const result=await db.insert(AIOutput).values({
+        formData:formData,
+        templateSlug:slug,
+        aiResponse:aiResp,
+        createdBy:user?.primaryEmailAddress?.emailAddress,
+        createdAt:moment().format('DD/MM/yyyy'),
+    });
+
+    console.log(result);
+}
   return (
     <div className="p-5">
       <Link href={"/dashboard"}>
@@ -33,11 +64,12 @@ function CreateNewContent(props: PROPS) {
         <FormSection
           selectedTemplate={selectedTemplate}
           userFormInput={(v: any) => GenerateAIContent(v)}
+          loading={loading}
         />
 
         {/* Output Section */}
         <div className="col-span-2">
-          <OutputSection />
+          <OutputSection aiOutput={aiOutput} />
         </div>
       </div>
     </div>
