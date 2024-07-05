2d73b65dc1bcc98394cfef691f25da46c1a20f50
Harshal Tupe <harshaltupe12@gmail.com>
2024-07-05 11:17:09 +0530

    Dynamic navigation

--- app/dashboard/_components/SideNav.tsx
+++ app/dashboard/_components/SideNav.tsx
@@ -1,54 +1,63 @@
-"use client"
-import { FileClock, Home, Settings, WalletCards } from 'lucide-react'
-import Link from 'next/link';
+"use client";
+import { FileClock, Home, Settings, WalletCards } from "lucide-react";
+import Link from "next/link";
 import React, { useEffect } from "react";
-import { usePathname } from 'next/navigation'
+import { usePathname } from "next/navigation";
+import UsageTrack from "./UsageTrack";
 function SideNav() {
-    const MenuList=[
-        {
-            name:'Home',
-            icon:Home,
-            path:'/dashboard'
-        },
-        {
-            name:'History',
-            icon:FileClock,
-            path:'/dashboard/history'
-        },
-        {
-            name:'Billing',
-            icon:WalletCards,
-            path:'/dashboard/billing'
-        },
-        {
-            name:'Setting',
-            icon:Settings,
-            path:'/dashboard/settings'
-        },
+  const MenuList = [
+    {
+      name: "Home",
+      icon: Home,
+      path: "/dashboard",
+    },
+    {
+      name: "History",
+      icon: FileClock,
+      path: "/dashboard/history",
+    },
+    {
+      name: "Billing",
+      icon: WalletCards,
+      path: "/dashboard/billing",
+    },
+    {
+      name: "Setting",
+      icon: Settings,
+      path: "/dashboard/settings",
+    },
+  ];
 
-    ]
+  const path = usePathname();
+  useEffect(() => {
+    console.log(path);
+  }, []);
 
-    const path = usePathname();
-    useEffect(()=>{
-      console.log(path)
-    },[])
   return (
-    <div className="h-screen p-5 shadow-sm border">
+    <div className="h-screen p-5 relative shadow-sm border">
       <div className="flex justify-center">
         <h2 className="font-bold text-3xl font-serif text-primary">
           Hyper Serve
         </h2>
       </div>
-      <hr className='my-6 border'/>
+      <hr className="my-6 border" />
       <div className="mt-3">
-      {
-        MenuList.map((menu, index)=>(
-            <div className={`flex gap-2 mb-2 p-3 hover:bg-primary hover:text-white rounded-lg transition-all cursor-pointer items-center ${path == menu.path&&'bg-primary text-white'}`} key={index}>
-                <menu.icon className=" h-6 w-6"/>
-                <h2 className='text-lg'>{menu.name}</h2>
+        {MenuList.map((menu, index) => (
+          <Link href={menu.path}>
+            <div
+              className={`flex gap-2 mb-2 p-3 hover:bg-primary hover:text-white rounded-lg transition-all cursor-pointer items-center ${
+                path == menu.path && "bg-primary text-white"
+              }`}
+              key={index}
+            >
+              <menu.icon className=" h-6 w-6" />
+              <h2 className="text-lg">{menu.name}</h2>
             </div>
-        ))
-      }
+          </Link>
+        ))}
+      </div>
+      <div className="absolute bottom-10 left-0 w-full">
+        <UsageTrack />
       </div>
     </div>
   );
