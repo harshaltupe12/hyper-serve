985c4596abaccc486fd3b0c72dfe399dd73c4838
Harshal Tupe <harshaltupe12@gmail.com>
2024-07-05 11:18:42 +0530

    getting total credits and display them

--- app/dashboard/content/[template-slug]/page.tsx
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import FormSection from "../_component/FormSection";
 import OutputSection from "../_component/OutputSection";
 import { TEMPLATE } from "../../_components/TemplateListSection";
@@ -12,6 +12,8 @@ import { db } from "@/utils/db";
 import { AIOutput } from "@/utils/schema";
 import { useUser } from "@clerk/nextjs";
 import moment from "moment";
+import { TotalUsageContext } from "@/app/(context)/TotalUsageContext";
+import { useRouter } from "next/navigation";
 
 interface PROPS {
   params: {
@@ -25,32 +27,43 @@ function CreateNewContent(props: PROPS) {
   );
 
   const [loading, setLoading] = useState(false);
-  const [aiOutput, setAiOutput] = useState<string>('');
-  const {user} = useUser();
+  const [aiOutput, setAiOutput] = useState<string>("");
+  const { user } = useUser();
+  const router = useRouter()
+  const { totalUsage, setTotalUsage } = useContext(TotalUsageContext);
 
-  const GenerateAIContent = async(formData: any) => {
+  const GenerateAIContent = async (formData: any) => {
+    if (totalUsage >= 10000) {
+      console.log("Please Upgrade");
+      router.push('/dashboard/billing')
+      return;
+    }
     setLoading(true);
-    const SelectedPrompt=selectedTemplate?.aiPrompt;
-    const FinalAIPrompt = JSON.stringify(formData)+", "+SelectedPrompt;
+    const SelectedPrompt = selectedTemplate?.aiPrompt;
+    const FinalAIPrompt = JSON.stringify(formData) + ", " + SelectedPrompt;
 
-    const result=await chatSession.sendMessage(FinalAIPrompt);
+    const result = await chatSession.sendMessage(FinalAIPrompt);
     console.log(result.response.text());
-    setAiOutput(result?.response.text())
-    await SaveInDb(JSON.stringify(formData),selectedTemplate?.slug,result?.response.text())
+    setAiOutput(result?.response.text());
+    await SaveInDb(
+      JSON.stringify(formData),
+      selectedTemplate?.slug,
+      result?.response.text()
+    );
     setLoading(false);
   };
 
-  const SaveInDb=async(formData:any,slug:any,aiResp:string)=>{
-    const result=await db.insert(AIOutput).values({
-        formData:formData,
-        templateSlug:slug,
-        aiResponse:aiResp,
-        createdBy:user?.primaryEmailAddress?.emailAddress,
-        createdAt:moment().format('DD/MM/yyyy'),
+  const SaveInDb = async (formData: any, slug: any, aiResp: string) => {
+    const result = await db.insert(AIOutput).values({
+      formData: formData,
+      templateSlug: slug,
+      aiResponse: aiResp,
+      createdBy: user?.primaryEmailAddress?.emailAddress,
+      createdAt: moment().format("DD/MM/yyyy"),
     });
 
     console.log(result);
-}
+  };
   return (
     <div className="p-5">
       <Link href={"/dashboard"}>
