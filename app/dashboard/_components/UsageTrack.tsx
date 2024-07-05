20cd686b49103c21c9fb5f373d9cb085116ec4ed
Harshal Tupe <harshaltupe12@gmail.com>
2024-07-05 23:23:11 +0530

    Wrap component with Context

--- app/dashboard/_components/UsageTrack.tsx
+++ app/dashboard/_components/UsageTrack.tsx
@@ -1,22 +1,32 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { db } from "@/utils/db";
-import { AIOutput } from "@/utils/schema";
+import { AIOutput, UserSubscription } from "@/utils/schema";
 import { useUser } from "@clerk/nextjs";
 import { eq } from "drizzle-orm";
 import React, { useContext, useEffect, useState } from "react";
 import { HISTORY } from "../history/page";
 import { TotalUsageContext } from "@/app/(context)/TotalUsageContext";
 import Link from "next/link";
+import { UserSubscriptionContext } from "@/app/(context)/UserSubscriptionContext";
+import { UpdateCreditUsageContext } from "@/app/(context)/UpdateCreditUsageContext";
 
 function UsageTrack() {
   const { user } = useUser();
   const {totalUsage, setTotalUsage} = useContext(TotalUsageContext)
+  const {userSubscription,setUserSubscription} = useContext(UserSubscriptionContext);
+  const [maxWords,setMaxWords]=useState(10000)
+  const {updateCreditUsage,setUpdateCreditUsage}=useContext(UpdateCreditUsageContext)
 
   
   useEffect(() => {
    user&&GetData();
+   user&&IsUserSubscribe();
   }, [user]);
+
+  useEffect(()=>{
+    user&&GetData();
+  },[updateCreditUsage&&user]);
 
   const GetData = async()=>{
     {/*@ts-ignore*/ }
@@ -26,6 +36,16 @@ function UsageTrack() {
     .where(eq(AIOutput.createdBy, user?.primaryEmailAddress?.emailAddress));
     GetTotalUsage(result)
 
+  }
+
+  const IsUserSubscribe=async()=>{
+     {/* @ts-ignore */}
+     const result=await db.select().from(UserSubscription).where(eq(UserSubscription.email,user?.primaryEmailAddress?.emailAddress));
+     if(result){
+      setUserSubscription(true)
+      setMaxWords(1000000);
+    }
+     console.log(result)
   }
 
   const GetTotalUsage = (result:HISTORY[]) => {
@@ -45,11 +65,11 @@ function UsageTrack() {
           <div
             className="h-2 bg-white rounded-full transition-all duration-1000"
             style={{
-              width:(totalUsage/10000)*100+"%",
+              width:(totalUsage/maxWords)*100+"%",
             }}
           ></div>
         </div>
-        <h2 className="text-sm">{totalUsage}/10,000 Credit use</h2>
+        <h2 className="text-sm">{totalUsage}/{maxWords} Credit use</h2>
       </div>
       <Link href={'/dashboard/billing'}>
       <Button variant={"secondary"} className="w-full my-2 text-primary">
